ade4017e5e970a0194315f258b3f2e689c099a5c
Aryeh I. Braid David <aryebdavid@gmail.com>
2022-04-14 21:38:00 -0300

    feat: style with tailwindcss

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 export default function ProductCard() {
   return (
-    <>
-    </>
+    <div>
+      <h4>Product</h4>
+      <div>
+        <img src="" alt="" />
+        <div>
+          <span>
+            Seda Brown
+          </span>
+          <span>
+            Smoking
+          </span>
+        </div>
+      </div>
+    </div>
   );
 }
