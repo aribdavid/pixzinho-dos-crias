338ab221d0b1813e025532390b72b5b560886cbd
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-06-29 14:37:28 -0300

    feat changed tab icon

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -9,7 +9,7 @@ export default function LoginPage() {
   return (
     <body className="bg-palette-secondary font-family-karla h-screen">
 
-      <div className="w-full flex flex-wrap">
+      <form action="/auth" method="post" className="w-full flex flex-wrap">
         <div className="w-full md:w-1/2 flex flex-col">
 
           <div className="flex justify-center md:justify-start pt-12 md:pl-12 md:-mb-24">
@@ -21,16 +21,16 @@ export default function LoginPage() {
             <form className="flex flex-col pt-3 md:pt-8">
               <div className="flex flex-col pt-4">
                 <span htmlFor="email" className="text-lg text-palette-primary font-bold">Email</span>
-                <input type="email" id="email" placeholder="your@email.com" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" />
+                <input type="email" name="user_email" id="email" placeholder="your@email.com" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" />
               </div>
 
               <div className="flex flex-col pt-4">
                 <span htmlFor="password" className="text-lg text-palette-primary font-bold">Senha</span>
-                <input type="password" id="password" placeholder="Password" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" />
+                <input type="password" name="user_password" id="password" placeholder="Password" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" />
               </div>
 
               <button
-                type="button"
+                type="submit"
                 className="pt-3 pb-2 bg-palette-primary text-white w-full mt-2 rounded-sm font-primary font-semibold text-xl flex
         justify-center items-baseline  hover:bg-palette-light hover:text-palette-primary"
               >
@@ -50,7 +50,7 @@ export default function LoginPage() {
         <div className="w-1/2 shadow-2xl">
           <img className="object-cover w-full h-screen hidden md:block" src={logo5} alt="img" />
         </div>
-      </div>
+      </form>
 
     </body>
   );
